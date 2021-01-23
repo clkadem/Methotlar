460642e5cae2f341bf17b365530fa6b4da125d48
Adem Çelik <clk.ademm@gmail.com>
2021-01-23 23:53:49 +0300

    23.01.2021

---
+++ OOP3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();            
+            IKrediManager tasitKrediManager = new TasitKrediManager();            
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLogggerService();
+
+            basvuruManager.BasvuruYap(esnafKrediManager,smsLoggerService);
+            
+            List<IKrediManager> krediler = new List<IKrediManager> {ihtiyacKrediManager,tasitKrediManager,konutKrediManager };
+           
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
